84b3d2f236de7fea22a1507d962f76b65b586cc7
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-10-28 15:41:29 -0400

    optimize

--- Problem9/SpecPythagTriplet.js
+++ Problem9/SpecPythagTriplet.js
@@ -7,7 +7,21 @@
 // There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 // Find the product abc.
 
+function checkAdd(a, b, c){
+    if (a+b+c == 1000){return true;}
+    return false;
+}
+
 function pythagTriplet(){
+    let b= 0, c = 0;
+    for (let a = 1 ; a < 1000 ; a++){
+        for (let b = a+1 ; b < 1000-a-c ; b++){
+            c = Math.sqrt(a*a + b*b);
+            if (Number.isInteger(c) && checkAdd(a, b, c)){
+                return a*b*c;
+            }
+        }
+    }
 }
 
 console.time("runtime");
