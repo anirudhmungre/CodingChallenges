720d4647bb9ef999e7992b70158821d64f6caeb3
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-10-28 15:50:59 -0400

    setup question

---
+++ Problem10/SumPrimes.js
@@ -0,0 +1,11 @@
+// Project Euler Question #10
+// The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+// Find the sum of all the primes below two million.
+
+function sumPrimes(max){
+    console.log(max);
+}
+
+console.time("runtime");
+console.log(sumPrimes(2e6));
+console.timeEnd("runtime");
