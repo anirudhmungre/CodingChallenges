2996222434aaab8280b5da6b6bc8f4e67fa5a525
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-10-28 00:03:43 -0400

    efficiency

--- Problem3/LargestPrimeFactor.py
+++ Problem3/LargestPrimeFactor.py
@@ -1,9 +1,11 @@
 import math
 
 n = 600851475143
-i = 2
+if n%2 == 0:
+    n /= 2
+i = 3
 while i < math.sqrt(n):
-    while n % i == 0:
-        n = n / i
-    i += 1
+    while n%i == 0:
+        n /= i
+    i += 2 # Increase by 2 because two is the lowest non-prime
 print (int(n))
