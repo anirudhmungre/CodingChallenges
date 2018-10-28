35888fdbab7e0ee9483e00ac4fbf41fb47535753
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-10-28 16:08:01 -0400

    solved

--- Problem10/SumPrimes.js
+++ Problem10/SumPrimes.js
@@ -2,13 +2,21 @@
 // The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 // Find the sum of all the primes below two million.
 
-function isPrime(){
-    
+function isPrime(x){
+    rootX = parseInt(Math.sqrt(x));
+    if (x%2 == 0){return false;}
+    for (let i = 3 ; i<rootX+1 ; i+=2){
+        if (x%i == 0){return false;}
+    }
+    return true;
 }
 
 function sumPrimes(max){
-    for (let i = 0 ; i<max ; i+=2){
-        if (isPrime()){sum += i;}
+    let sum = 2;
+    for (let i = 3 ; i<max ; i+=2){
+        if (isPrime(i)){
+            sum += i;
+        }
     }
     return sum;
 }
