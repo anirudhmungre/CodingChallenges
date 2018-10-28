5045243064baeec9868e46def2b48471a37c03ac
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-10-28 11:44:30 -0400

    brute force

---
+++ Problem4/LargePalindroneProd.js
@@ -0,0 +1,24 @@
+// Largest product of two 3 digit nums is 6 digits
+function isPalindrome(mul){
+    let check = mul.toString();
+    let temp = check.split("").reverse().join("")
+    if (check == temp){
+        return true;
+    } 
+    else{return false;}
+}
+
+function iterate(){
+    let largest = -1;
+    for (let i = 999 ; i > 100 ; i--){
+        for (let j = 999 ; j > 100 ; j--){
+            // console.log(i + "   " + j);
+            // console.log(i*j);
+            if (isPalindrome(i*j) && (i*j)>largest){
+                largest = i*j;
+            }
+        }
+    }    
+    return largest;
+}
+console.log(iterate());
