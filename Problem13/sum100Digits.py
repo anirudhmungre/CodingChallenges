b1153ddba1866017db82223efb40526717f4ac4d
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-11-06 12:07:15 -0500

    setup problem

---
+++ Problem13/sum100Digits.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import time as t
+# Project Euler Question #13
+# Work out the first ten digits of the sum of the following one-hundred 50-digit numbers. (in input.txt)
+
+def foo():
+    return
+
+start = t.time()
+print(foo())
+print("Runtime: {} Seconds".format(t.time()-start))
